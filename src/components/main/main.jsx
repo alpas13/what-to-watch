1cc1a48dce94e4c5ee844ad3400f72ae843d483e
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-04-14 13:43:03 +0200

    Fixed Main component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {Fragment} from "react";
 import PageContent from "../page-content/page-content.jsx";
 import Genre from "../genre/genre.jsx";
 import FilmsList from "../films-list/films-list.jsx";
 import ShowMoreButton from "../show-more-button/show-more-button.jsx";
+import MovieCard from "../movie-card/movie-card.jsx";
+import Footer from "../footer/footer.jsx";
+import MovieCardWrapper from "../movie-card-wrapper/movie-card-wrapper.jsx";
 
 const Main = React.memo(function Main() {
   return (
-      <PageContent>
-        <Genre />
-        <FilmsList />
-        <ShowMoreButton />
-      </PageContent>
+      <Fragment>
+        <MovieCardWrapper>
+          <MovieCard />
+        </MovieCardWrapper>
+        <PageContent>
+          <Genre/>
+          <FilmsList/>
+          <ShowMoreButton/>
+          <Footer/>
+        </PageContent>
+      </Fragment>
   );
 });
 
