d966b17acd146e1826e018f9cd82d6c9ab2ba436
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-04-14 13:40:04 +0200

    Created MovieCard and PageContent components

--- src/components/page-content/page-content.jsx
+++ src/components/page-content/page-content.jsx
@@ -3,14 +3,18 @@ import PropTypes from "prop-types";
 
 const PageContent = React.memo(function PageContent(props) {
   const {children} = props;
-  const isMain = React.Children.count(children) > 1;
+  const childrenCount = React.Children.count(children);
+  const isMain = childrenCount > 2;
   return (
       <div className="page-content">
         <section className="catalog">
           {isMain && <h2 className="catalog__title visually-hidden">Catalog</h2>}
           {isMain || <h2 className="catalog__title">More like this</h2>}
-          {children}
+          {React.Children.map(children, (node, index) => {
+            return isMain && index !== children.length -1 ? node : ``;
+          })}
         </section>
+        {isMain ? children[children.length -1] : ``}
       </div>
   );
 });
