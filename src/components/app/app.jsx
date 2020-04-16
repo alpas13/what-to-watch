7fb18b0a0c407f5f984fb255652398290dbb3169
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-04-16 18:10:45 +0200

    Added mocks data and passed it to SmallMovieCard component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,10 @@
-import React, {Fragment} from "react";
+import React from "react";
 import Main from "../main/main.jsx";
-import MoviePage from "../movie-page/movie-page.jsx";
-import MyList from "../my-list/my-list.jsx";
-import Player from "../player/player.jsx";
-import AddReview from "../add-review/add-review.jsx";
-import AuthScreen from "../auth-screen/auth-screen.jsx";
 
 const App = React.memo(function App() {
-  const cur = `authScreen`;
 
   return (
-    <Fragment>
-      {cur === `moviePage` && <MoviePage
-        currentTab={`overview`}
-      />}
-      {cur === `main` && <Main/>}
-      {cur === `myList` && <MyList/>}
-      {cur === `player` && <Player/>}
-      {cur === `addReview` && <AddReview/>}
-      {cur === `authScreen` && <AuthScreen/>}
-    </Fragment>
+     <Main />
   );
 });
 
