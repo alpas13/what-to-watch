cc659dc3005ecd4580974e1189033779572555f7
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2020-04-17 20:25:51 +0200

    Fixed tests

--- src/components/films-list/films-list.test.js
+++ src/components/films-list/films-list.test.js
@@ -1,12 +1,38 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import FilmsList from "./films-list.jsx";
-import PreviewFilm from "../preview-film/preview-film.jsx";
+import SmallMovieCard from "../small-movie-card/small-movie-card.jsx";
+
+const mock = [
+  {
+    id: 1,
+    name: `Shutter Island`,
+    posterImage: `img/shutter-island-poster.jpg`,
+    previewImage: `img/shutter-island.jpg`,
+    backgroundImage: `img/shutter-island-bg.jpg`,
+    backgroundColor: `#ffffff`,
+    videoLink: `https://some-link`,
+    previewVideoLink: `https://some-link`,
+    description: `In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.`,
+    rating: 8.1,
+    scoresCount: 610,
+    director: `Martin Scorsese`,
+    starring: [`Leonardo DiCaprio`, `Emily Mortimer`, `Mark Ruffalo`],
+    runTime: 138,
+    genre: `Mystery`,
+    released: 2010,
+    isFavorite: false
+  }
+];
 
 test(`Correctly render FilmsList component`, () => {
+  const {films} = mock;
+
   const tree = renderer.create(
       <FilmsList>
-        <PreviewFilm />
+        <SmallMovieCard
+            films={films}
+        />
       </FilmsList>
   ).toJSON();
 
